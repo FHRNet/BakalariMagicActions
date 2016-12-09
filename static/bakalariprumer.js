c25f2e3c5c3b58dd089f3d3ef5cc8fa263c65e4d
vojta001 <vojtech.kane@gmail.com>
2016-12-09 22:28:28 +0100

    Ability to disable an existing mark

--- static/bakalariprumer.js
+++ static/bakalariprumer.js
@@ -27,7 +27,7 @@ function calculateWeightedAvg() {
 	
 
 		jQuery(this).find(".typ").each(function() {
-			jQuery(this).find("td").each(function() {
+			jQuery(this).find("td").not(".disabled").each(function() {
 				vaha = Number(jQuery(this).text().replace(/U/g,6).replace(/X/g,10).replace(/R/g,3).replace(/P/g,4).replace(/C/g,10));
 				vahy.push(vaha);
 				sumvahy += vaha;
@@ -36,7 +36,7 @@ function calculateWeightedAvg() {
 		});
 	
 		jQuery(this).find(".detznamka").each(function() {
-			jQuery(this).find("td").each(function() {
+			jQuery(this).find("td").not(".disabled").each(function() {
 				znamka = Number(jQuery(this).text().replace(/X/g,0).replace(/\?/g,0).replace(/U/g,0).replace(/N/g,0).replace(/A/g,0).replace(/1-/g,1.5).replace(/2-/g,2.5).replace(/3-/g,3.5).replace(/4-/g,4.5).replace(/-/g,0));
 				znamky.push(znamka);
 				sumznamky += znamka;
@@ -84,15 +84,24 @@ function addznamka(element) {
 }
 
 function modifUI() {
-        jQuery(".radekznamky").children("tbody").children("tr").each(function() {
-                jQuery(this).append(
-		"<table cellpadding='0' cellspacing='0' border='0'><tbody><tr><td>" +
-		"<table cellpadding='0' cellspacing='0' border='0'><tbody>" +
-		"<tr><td><input style='position:relative;width:35px;height:13px;margin:0px;' class='inznamka' type='number' min='1' max='5' step='1' value='1'></td></tr>" +
-                "<tr><td><input style='position:relative;width:35px;height:13px;margin:0px;' class='intyp' type='number' min='1' max='10' step='1' value='1'></td></tr>" +
-		"</tbody></table>" +
-		"</td><td><button style='height:26px;width:26px;' onclick='addznamka(this);return false;'>+</button></td></tr></tbody></table>");
-        });
+	jQuery(".radekznamky").children("tbody").children("tr").each(function() {
+		jQuery(this).append(
+			"<table cellpadding='0' cellspacing='0' border='0'><tbody><tr><td>" +
+			"<table cellpadding='0' cellspacing='0' border='0'><tbody>" +
+			"<tr><td><input style='position:relative;width:35px;height:13px;margin:0px;' class='inznamka' type='number' min='1' max='5' step='1' value='1'></td></tr>" +
+			"<tr><td><input style='position:relative;width:35px;height:13px;margin:0px;' class='intyp' type='number' min='1' max='10' step='1' value='1'></td></tr>" +
+			"</tbody></table>" +
+			"</td><td><button style='height:26px;width:26px;' onclick='addznamka(this);return false;'>+</button></td></tr></tbody></table>");
+		jQuery(this).find(".detznamka td").each(function() {
+			jQuery(this).click(function() {
+				jQuery(this).toggleClass("disabled");
+				var index = jQuery(this).index();
+				jQuery(this).parent().parent().parent().find(".typ td:eq("+index+")").toggleClass("disabled");
+				calculateWeightedAvg();
+			});
+		});
+	});
+	jQuery("head").append("<style>.disabled{text-decoration:line-through;opacity:0.5;}");
 }
 
 function removeZnamka(id) {
