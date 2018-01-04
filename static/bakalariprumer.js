090205ff402eb2333c98980cfd601432c96019e0
Greenscreener <honzikcernoh@gmail.com>
2018-01-04 10:18:26 +0100

    Added option to add cutom marks.

--- static/bakalariprumer.js
+++ static/bakalariprumer.js
@@ -27,7 +27,7 @@ function calculateWeightedAvg() {
 		let sumvahy	= 0;
 		let sum		= 0;
 		let wavg	= 0;
-	
+
 
 		jQuery(this).find(".znamka-v").each(function() {
 			let znamka = jQuery(this).data("clasif");
@@ -39,8 +39,8 @@ function calculateWeightedAvg() {
 			znamky.push(znamka);
 			sumznamky += znamka;
 		});
-	
-	
+
+
 		jQuery.each(znamky, function(id, zn) {
 			zn = Number(zn);
 			if(zn !== 0) {
@@ -60,12 +60,21 @@ function calculateWeightedAvg() {
 	console.log("Recalculated averages");
 }
 function addMark (znamka,vaha,element) {
-	jQuery(element).append('<div onclick="this.parentElement.removeChild(this); calculateWeightedAvg();" class="znamka-v" data-clasif="{&quot;vaha&quot;:' + vaha + ',&quot;MarkText&quot;:&quot;' + znamka + '&quot;}" style="background-color: #dfd; float: left; list-style: none; position: relative; width: 56px;"> <div class="cislovka maly"> <div class="ob">' + znamka + '</div> </div> <div class="bod"></div> <div class="dodatek"> <span>' + vaha + '</span><br>28.11.17 </div> </div>');
+	znamka = Math.round(znamka);
+	vaha = Math.round(vaha);
+	if (znamka > 5) {znamka = 5;}
+	if (znamka < 1) {znamka = 1;}
+	if (vaha > 10) {vaha = 10;}
+	if (vaha < 1) {vaha = 1;}
+	jQuery(element).append('<div onclick="this.parentElement.removeChild(this); calculateWeightedAvg();" class="znamka-v" data-clasif="{&quot;vaha&quot;:' + vaha + ',&quot;MarkText&quot;:&quot;' + znamka + '&quot;}" style="background-color: #dfd; float: left; list-style: none; position: relative; width: 56px;"> <div class="cislovka stredni"> <div class="ob">' + znamka + '</div> </div> <div class="bod"></div> <div class="dodatek"> <span>' + vaha + '</span><br>Právě teď </div> </div>');
 	calculateWeightedAvg();
 }
-
+function addMarkAdders () {
+	jQuery(".znamky").prepend('<div class="znamka-v" data-clasif="{&quot;vaha&quot;:0,&quot;MarkText&quot;:&quot;0&quot;}" style=" float: left; list-style: none; position: relative; width: 56px;"> <div class="cislovka stredni"> <div class="ob"> <input class="nova-znamka" type="number" min="1" max="5" maxlength="1" style="font-size: 17px; width: 2em;"> </div> </div> <div class="bod"><input type="button" onclick="addMark(jQuery(this).parents(&quot;.znamka-v&quot;).find(&quot;.nova-znamka&quot;)[0].value,jQuery(this).parents(&quot;.znamka-v&quot;).find(&quot;.nova-vaha&quot;)[0].value,jQuery(this).parents(&quot;.znamky&quot;));jQuery(this).parents(&quot;.znamka-v&quot;).find(&quot;.nova-znamka&quot;)[0].value=&quot;&quot;;jQuery(this).parents(&quot;.znamka-v&quot;).find(&quot;.nova-vaha&quot;)[0].value = &quot;&quot;;" value="+"></div> <div class="dodatek"> <span> <input type="number" class="nova-vaha" min="1" max="10" maxlength="1" style="font-size: 15px; width: 2em;"> </span><br></div> </div>');
+}
 jQuery(document).ready(function() {
 	if(window.location.href.indexOf("prubzna.aspx") > -1) {
 		calculateWeightedAvg();
+		addMarkAdders();
 	}
 });
