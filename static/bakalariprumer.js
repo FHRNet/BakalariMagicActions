099c4291ab1f20848f23757db8e5287830cf9357
Greenscreener <honzikcernoh@gmail.com>
2020-01-31 18:08:25 +0100

    Rewrite bakalariprumer.js

--- static/bakalariprumer.js
+++ static/bakalariprumer.js
@@ -2,81 +2,93 @@
 
 /*
 Name: Bakalari Actions
-Author: FHR; fhrnet.eu
-Version: 1.3
+Author: Greenscreener; grnscrnr.tk
+Version: 2.0
 Licence: GPL v3
 
-This version works with "Next"
+This version works with "Next", now a little better.
 */
 
 
-var znamka	= 0;
-var vaha	= 0;
-var lastid	= 0;
+document.querySelectorAll(".info-text").forEach(e => {
+    e.style.width = "20em";
+    e.innerHTML = `
+        <div class="pridavac-znamek"> 
 
+            <input type="number" min="1" max="5" step="0.5" required class="znamka-input" style="width: 2em; height: 2.5em;">
+            <input type="number" min="1" max="10" step="1" required class="vaha-input" style="width: 2em; height: 2.5em;">
+            <button data-id="${e.dataset.id}" style="width: 2.5em; height: 2.5em;">+</button>
+        </div>               
+        `;
+});
+document.querySelectorAll(".pridavac-znamek").forEach(e => e.onclick = (e) => {e.stopPropagation()});
+document.querySelectorAll(".pridavac-znamek button").forEach(e => {
+    e.addEventListener("click", (event) => {
+        event.preventDefault();
+        const radek = document.querySelector("#" + e.dataset.id);
+        const znamky = radek.querySelector(".znamky");
+        const znamkaDiv = document.createElement("div");
+        const znamka = radek.querySelector(".znamka-input").value;
+        const vaha = radek.querySelector(".vaha-input").value;
+        if (vaha === "") {
+            e.parentElement.querySelector("input").focus();
+            return;
+        }
+        znamkaDiv.innerHTML = `
+            <div class='cislovka stredni'>
+                <div class='ob'>${znamka.toString().replace(".5","-")}</div>
+            </div> 
+            <div class='bod'>
+                
+            </div> 
+            <div class='dodatek'>
+                <p>${vaha}</p>
+                <p>Právě teď </p>
+            </div> 
+        `;
+        znamkaDiv.style.backgroundColor = '#dfd';
+        znamkaDiv.style.float = 'left';
+        znamkaDiv.style.listStyleType = 'none';
+        znamkaDiv.style.position = 'relative';
+        znamkaDiv.style.width = '56px';
+    
+        znamkaDiv.dataset.clasif = `
+        {
+            "vaha": ${vaha},
+            "MarkText": "${znamka.toString().replace(".5","-")}"
+        }
+        `;
+        znamkaDiv.dataset.vaha = vaha;
+        znamkaDiv.dataset.znamka = znamka;
+        
+        znamkaDiv.classList.add("znamka-v");
+        
+        znamkaDiv.addEventListener("click", (e) => {
+            znamkaDiv.parentElement.removeChild(znamkaDiv);
+            calculateWeightedAvg();
+        });
+        
+        znamky.append(znamkaDiv);
+        calculateWeightedAvg();
+        window.dispatchEvent(new Event('resize'));
+    });
+});
 
 function calculateWeightedAvg() {
-	if(jQuery('.prumer').length) {
-		jQuery('.prumer').remove();
-	}
-
-	jQuery(".bx-viewport").children(".znamky").each(function() {
-		let znamky 	= [];
-		let vahy 	= [];
-		let sumznamky 	= 0;
-		let sumvahy	= 0;
-		let sum		= 0;
-		let wavg	= 0;
-
-
-		jQuery(this).find(".znamka-v").each(function() {
-			let znamka = jQuery(this).data("clasif");
-
-			vahy.push(znamka.vaha);
-			sumvahy += (znamka.vaha);
-
-			znamka = Number(znamka.MarkText.replace(/X/g,0).replace(/\?/g,0).replace(/U/g,0).replace(/N/g,0).replace(/A/g,0).replace(/1-/g,1.5).replace(/2-/g,2.5).replace(/3-/g,3.5).replace(/4-/g,4.5).replace(/-/g,0).replace(/!$/,''));
-			znamky.push(znamka);
-			sumznamky += znamka;
-		});
-
-
-		jQuery.each(znamky, function(id, zn) {
-			zn = Number(zn);
-			if(zn !== 0) {
-				sum += (zn * vahy[id]);
-			} else {
-				sumvahy -= vahy[id];
-			}
-		});
-		wavg = (Math.round(sum/sumvahy*100)/100).toFixed(2);
-		if (isNaN(wavg)) {
-			wavg = 0;
-		}
-
-		jQuery(this).parent().parent().parent().find(".obal").append(jQuery('<h2 title="Vazeny prumer" style="color: blueviolet;" class="prumer">' + wavg + '</h2>'));
-	});
-
-	console.log("Recalculated averages");
+    document.querySelectorAll(".predmet-radek").forEach(e => {
+        let znamky = [...e.querySelectorAll(".znamka-v")];
+        znamky = znamky.map(e => {
+            const json = JSON.parse(e.dataset.clasif);
+            return {
+                znamka: parseFloat(json.MarkText.replace("-",".5")),
+                vaha: json.vaha
+            };
+        });
+        const sumznamky = znamky.reduce((a, v) => (a.hasOwnProperty("znamka") ? (isNaN(a.znamka) ? 0 : (a.znamka * a.vaha)) : a) + (isNaN(v.znamka) ? 0 : (v.znamka * v.vaha)));
+        const sumvahy = znamky.reduce((a, v) => (a.hasOwnProperty("vaha") ? (isNaN(a.znamka) ? 0 : (a.vaha)) : a) + (isNaN(v.znamka) ? 0 : (v.vaha)));
+        let prumer = sumznamky.hasOwnProperty("znamka") ? sumznamky.znamka : sumznamky / sumvahy;
+        prumer = Math.round(prumer * 100)/100;
+        e.querySelectorAll(".sim-avg").forEach(e => {e.parentElement.removeChild(e);});
+        e.querySelector(".dalsi-dva").innerHTML+=`<div class="sim-avg" style="color: darkred" title="vážený průměr s simulací známek">${prumer.toString().replace(".", ",")}</div>`;
+    });
 }
-function addMark (znamka,vaha,element) {
-	znamka = Math.round(znamka);
-	vaha = Math.round(vaha);
-	if (znamka > 5) {znamka = 5;}
-	if (znamka < 1) {znamka = 1;}
-	if (vaha > 10) {vaha = 10;}
-	if (vaha < 1) {vaha = 1;}
-	jQuery(element).append("<div onclick='this.parentElement.removeChild(this); calculateWeightedAvg(); window.dispatchEvent(new Event(\"resize\"));' class='znamka-v' data-clasif='{\"vaha\":" + vaha + ",\"MarkText\":\"" + znamka + "\"}' style='background-color: #dfd; float: left; list-style: none; position: relative; width: 56px;'> <div class='cislovka stredni'> <div class='ob'>" + znamka + "</div> </div> <div class='bod'></div> <div class='dodatek'> <span>" + vaha + "</span><br>Právě teď </div> </div>");
-	calculateWeightedAvg();
-	window.dispatchEvent(new Event('resize'));
-}
-function addMarkAdders () {
-	jQuery(".znamky").prepend("<div class='znamka-v' data-clasif='{\"vaha\":0,\"MarkText\":\"0\"}' style=' float: left; list-style: none; position: relative; width: 56px;'> <div class='cislovka stredni'> <div class='ob'> <input class='nova-znamka' type='number' min='1' max='5' maxlength='1' style='font-size: 17px; width: 2em;'> </div> </div> <div class='bod'><button class='ui-button ui-widget ui-corner-all' type='button' onclick='addMark(jQuery(this).parents(\".znamka-v\").find(\".nova-znamka\")[0].value,jQuery(this).parents(\".znamka-v\").find(\".nova-vaha\")[0].value,jQuery(this).parents(\".znamky\"));jQuery(this).parents(\".znamka-v\").find(\".nova-znamka\")[0].value=\"\";jQuery(this).parents(\".znamka-v\").find(\".nova-vaha\")[0].value = \"\";' style='font-size:11px; position: relative; top: -5px;' value='+'>+</button></div> <div class='dodatek'> <span> <input type='number' class='nova-vaha' min='1' max='10' maxlength='1' style='font-size: 15px; width: 2em;'> </span><br></div> </div>");
-	window.dispatchEvent(new Event('resize'));
-}
-jQuery(document).ready(function() {
-	if(window.location.href.indexOf("prubzna.aspx") > -1) {
-		setTimeout(calculateWeightedAvg,1);
-		addMarkAdders();
-	}
-});
